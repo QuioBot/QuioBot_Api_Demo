54404d61a496158986b83451c22d1d95f5d692f1
elghibaomar <elghibaomar@gmail.com>
2022-11-26 15:32:22 +0100

    l1

--- src/app/database.py
+++ src/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'postgresql://admin:password@localhost/fastapi_database'
+SQLALCHAMY_DATABASE_URL = 'postgresql://gdhwlficqahlpa:7260b7853dbf90d2e4e56834046cdbd6d402136cafc6fb25c0b53954372813ac@ec2-52-70-45-163.compute-1.amazonaws.com/d1cf63itopno05'
 
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
